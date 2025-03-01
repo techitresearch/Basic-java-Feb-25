77dfa88c2090b183f4ea14b64e370d3ec38b9887
techitresearch <techitresearch@gmail.com>
2025-03-01 20:16:59 +0530

    Update Hello World in printf

--- BasicJava/HelloWorld.java
+++ BasicJava/HelloWorld.java
@@ -3,6 +3,6 @@ package BasicJava;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello OOOO");
+        System.out.println("Hello OOOO dfbdfhd");
     }
 }
